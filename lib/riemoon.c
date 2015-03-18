b6ced83fcb07c9c2cca229dc47c878a5077a07fd
Gergely Nagy <algernon@madhouse-project.org>
2015-03-18 11:18:50 +0100

    Initial import
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

---
+++ lib/riemoon.c
@@ -0,0 +1,187 @@
+#include <riemann/riemann-client.h>
+#include <string.h>
+#include <stdio.h>
+
+#define LUA_LIB 1
+#include <lua.h>
+#include <lauxlib.h>
+
+typedef struct
+{
+  riemann_client_t *client;
+} riemoon_client_t;
+
+static int
+riemoon_destroy (lua_State *l)
+{
+  riemoon_client_t *client;
+
+  client = (riemoon_client_t *)luaL_checkudata (l, 1, "Riemoon");
+
+  if (client->client)
+    riemann_client_free (client->client);
+
+  return 0;
+}
+
+static int
+riemoon_send (lua_State *l)
+{
+  riemoon_client_t *client;
+  riemann_event_t *event;
+  riemann_message_t *message;
+
+  client = (riemoon_client_t *)luaL_checkudata (l, 1, "Riemoon");
+
+  luaL_checktype (l, 2, LUA_TTABLE);
+
+  event = riemann_event_new ();
+
+  lua_pushnil(l);
+  while (lua_next(l, -2) != 0) {
+    const char *key;
+
+    key = lua_tostring (l, -2);
+
+    if (strcmp (key, "time") == 0)
+      {
+        int64_t value = (int64_t) luaL_checklong (l, -1);
+
+        riemann_event_set_one (event, TIME, value);
+      }
+    else if (strcmp (key, "state") == 0)
+      {
+        const char *value = luaL_checkstring (l, -1);
+
+        riemann_event_set_one (event, STATE, value);
+      }
+    else if (strcmp (key, "service") == 0)
+      {
+        const char *value = luaL_checkstring (l, -1);
+
+        riemann_event_set_one (event, SERVICE, value);
+      }
+    else if (strcmp (key, "host") == 0)
+      {
+        const char *value = luaL_checkstring (l, -1);
+
+        riemann_event_set_one (event, HOST, value);
+      }
+    else if (strcmp (key, "description") == 0)
+      {
+        const char *value = luaL_checkstring (l, -1);
+
+        riemann_event_set_one (event, DESCRIPTION, value);
+      }
+    else if (strcmp (key, "tags") == 0)
+      {
+        int i, n;
+
+        luaL_checktype (l, -1, LUA_TTABLE);
+
+        n = (int)lua_rawlen (l, -1);
+
+        for (i = 1; i <= n; i++)
+          {
+            const char *value;
+
+            lua_rawgeti (l, -1, i);
+            value = luaL_checkstring (l, -1);
+
+            riemann_event_tag_add (event, value);
+
+            lua_pop (l, 1);
+          }
+
+      }
+    else if (strcmp (key, "ttl") == 0)
+      {
+        lua_Number value = luaL_checknumber (l, -1);
+
+        riemann_event_set_one (event, TTL, (float) value);
+      }
+    else if (strcmp (key, "metric") == 0)
+      {
+        lua_Number value = luaL_checknumber (l, -1);
+
+        riemann_event_set_one (event, METRIC_D, (double) value);
+      }
+    else
+      {
+        const char *value = luaL_checkstring (l, -1);
+
+        riemann_event_attribute_add (event,
+                                     riemann_attribute_create(key, value));
+      }
+
+    lua_pop(l, 1);
+  }
+
+  message = riemann_message_new ();
+  riemann_message_set_events (message, event, NULL);
+
+  riemann_client_send_message_oneshot (client->client, message);
+
+  return 0;
+}
+
+static int
+riemoon_connect (lua_State *l)
+{
+  riemoon_client_t *client;
+  const char *type_s, *host;
+  int port;
+  riemann_client_type_t type;
+
+  luaL_argcheck (l, lua_gettop (l) <= 3,
+                 1,
+                 "expected at most 3 arguments");
+
+  type_s = luaL_optstring (l, 1, "tcp");
+  host = luaL_optstring (l, 2, "localhost");
+  port = luaL_optinteger (l, 3, 5555);
+
+  if (strcmp (type_s, "tcp") == 0)
+    type = RIEMANN_CLIENT_TCP;
+  else if (strcmp (type_s, "udp") == 0)
+    type = RIEMANN_CLIENT_UDP;
+  else
+    luaL_error (l, "invalid riemann client type: %s", type_s);
+
+  lua_settop (l, 0);
+
+  lua_newtable (l);
+
+  client = (riemoon_client_t *)lua_newuserdata (l, sizeof (riemoon_client_t));
+  client->client = riemann_client_create (type, host, port);
+
+  luaL_getmetatable (l, "Riemoon");
+  lua_setmetatable (l, -2);
+
+  return 1;
+}
+
+int
+luaopen_riemoon (lua_State *l)
+{
+  luaL_Reg functions[] = {
+    {"connect", riemoon_connect},
+    {NULL, NULL}
+  };
+  luaL_Reg methods[] = {
+    {"send", riemoon_send},
+    {"__gc", riemoon_destroy},
+    {NULL, NULL}
+  };
+
+  luaL_newmetatable (l, "Riemoon");
+  lua_pushvalue (l, -1);
+
+  lua_setfield (l, -2, "__index");
+
+  luaL_setfuncs (l, methods, 0);
+
+  luaL_newlib (l, functions);
+
+  return 1;
+}

98bc5d400f43d06ec203116574d7db18f70058dc
Gergely Nagy <algernon@madhouse-project.org>
2015-03-19 09:55:40 +0100

    tests: Rewritten with Busted instead of LuaUnit
    Unlike LuaUnit, Busted is available via LuaRocks.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- tests/test_riemoon.lua
+++ tests/test_riemoon.lua
@@ -1,112 +1,151 @@
-#! /usr/bin/env lua
+#! /usr/bin/env busted
 
-require ('luaunit')
 fn, err = package.loadlib ("lib/.libs/riemoon.so", "luaopen_riemoon")
 if not fn then print (err)
 else
    riemoon = fn()
 end
 
-function test_riemoon_connect ()
-   -- Connection failure
-   client, errno, err = riemoon.connect ("tcp", "127.0.0.1", 6555)
-   assertNil (client)
-   assertEquals (errno, 111)
+describe (
+   "Riemoon /",
+   function ()
+      describe (
+         "Connecting /",
+         function ()
+            it (
+               "Should fail to connect to a bad port",
+               function ()
+                  client, errno, err = riemoon.connect ("tcp", "127.0.0.1", 6555)
+                  assert.is_nil (client)
+                  assert.are.equal (errno, 111)
+            end)
 
-   -- Successful connect
-   client, errno = riemoon.connect ("tcp", "127.0.0.1", 5555)
-   assertIsUserdata (client)
-   assertEquals (errno, 0)
+            it (
+               "Should connect to Riemann",
+               function ()
+                  client, errno = riemoon.connect ("tcp", "127.0.0.1", 5555)
+                  assert.is_userdata (client)
+                  assert.are.equal (errno, 0)
+            end)
 
-   -- Successful connect, default values
-   client, errno = riemoon.connect ()
-   assertIsUserdata (client)
-   assertEquals (errno, 0)
+            it (
+               "Should connect with default values",
+               function ()
+                  client, errno = riemoon.connect ()
+                  assert.is_userdata (client)
+                  assert.are.equal (errno, 0)
+            end)
 
-   -- UDP
-   client, errno, err = riemoon.connect ("udp")
-   assertIsUserdata (client)
-   assertEquals (errno, 0)
+            it (
+               "Should connect with UDP too",
+               function ()
+                  client, errno, err = riemoon.connect ("udp")
+                  assert.is_userdata (client)
+                  assert.are.equal (errno, 0)
+            end)
 
-   -- Invalid type
-   assertErrorMsgEquals ("invalid riemann client type: invalid",
-                         riemoon.connect, "invalid")
-end
+            it (
+               "Should error out on invalid connection type",
+               function ()
+                  local errfn = function ()
+                     riemoon.connect ("invalid")
+                  end
+                  assert.has_error (errfn, "invalid riemann client type: invalid")
+            end)
+      end)
 
-function test_riemann_send ()
-   client = riemoon.connect ()
+      describe (
+         "Sending events /",
+         function ()
+            client = riemoon.connect ()
 
-   errno, err = client:send()
-   assertEquals (errno, 0)
+            it (
+               "Empty events should be ok",
+               function ()
+                  errno, err = client:send ()
+                  assert.are.equal (errno, 0)
+            end)
 
-   errno, err = client:send({host = "localhost",
-                             service = "lua test",
-                             metric = 1,
-                             tags = {"foo","bar", "baz"},
-                             description = "A test message",
-                             time = 1,
-                             ttl = 60,
-                             something = "else"})
-   assertEquals (errno, 0)
+            it (
+               "Should be able to send a single event",
+               function ()
+                  errno, err = client:send({host = "localhost",
+                                            service = "lua test",
+                                            metric = 1,
+                                            tags = {"foo","bar", "baz"},
+                                            description = "A test message",
+                                            time = 1,
+                                            ttl = 60,
+                                            something = "else"})
+                  assert.are.equal (errno, 0)
+            end)
 
-   errno, err = client:send({host = "localhost",
-                             service = "lua test",
-                             metric = 1,
-                             tags = {"foo","bar", "baz"},
-                             something = "else"},
-                            {host = "localhost",
-                             service = "lua test #2",
-                             metric = 1,
-                             tags = {"foo","bar", "baz"},
-                             something = "else"})
-   assertEquals (errno, 0)
-end
+            it (
+               "Should be able to send multiple events",
+               function ()
+                  errno, err = client:send({host = "localhost",
+                                            service = "lua test",
+                                            metric = 1,
+                                            tags = {"foo","bar", "baz"},
+                                            something = "else"},
+                                           {host = "localhost",
+                                            service = "lua test #2",
+                                            metric = 1,
+                                            tags = {"foo","bar", "baz"},
+                                            something = "else"})
+                  assert.are.equal (errno, 0)
+            end)
+      end)
 
-function test_riemann_query ()
-   client = riemoon.connect ("tcp", "127.0.0.1", 5555)
+      describe (
+         "Queries /",
+         function ()
+            client = riemoon.connect ("tcp", "127.0.0.1", 5555)
 
-   client:send ({service = "riemoon query test",
-                 state = "ok",
-                 tags = {"riemoon"},
-                 riemoon = "yes"})
+            client:send ({service = "riemoon query test",
+                          state = "ok",
+                          tags = {"riemoon"},
+                          riemoon = "yes"})
 
-   client = riemoon.connect ("tcp", "127.0.0.1", 5555)
+            client = riemoon.connect ("tcp", "127.0.0.1", 5555)
 
-   data, errno, err = client:query ("error =")
-   assertNil (data)
-   assertEquals (errno, -1)
+            it (
+               "Should return nil and the error when given an invalid query",
+               function ()
+                  data, errno, err = client:query ("error =")
+                  assert.is_nil (data)
+                  assert.are.equal (errno,  -1)
+            end)
 
-   data, errno, err = client:query ("service = \"riemoon query test\"")
-   assertNotNil (data)
-   assertEquals (errno, 0)
-   assertEquals (#data, 1)
-   assertEquals (data[1].service, "riemoon query test")
-   assertEquals (data[1].state, "ok")
-   assertEquals (data[1].riemoon, "yes")
-   assertEquals (data[1].tags[1], "riemoon")
-   assertEquals (data[1].attributes['riemoon'], "yes")
-   assertNil (data[1].description)
-   assertNil (data[1].host)
-   assertIsNumber (data[1].ttl)
-   assertIsNumber (data[1].time)
-   assertIsNumber (data[1].metric)
-   assertNil (data[1].unknown)
-   assertNil (data[2])
+            it (
+               "Should return events when querying for known data",
+               function ()
+                  data, errno, err = client:query ("service = \"riemoon query test\"")
 
-   data = nil
-   collectgarbage ()
+                  assert.is_not_nil (data)
+                  assert.are.equal (errno, 0)
+                  assert.are.equal (#data, 1)
+                  assert.are.equal (data[1].service, "riemoon query test")
+                  assert.are.equal (data[1].state, "ok")
+                  assert.are.equal (data[1].riemoon, "yes")
+                  assert.are.equal (data[1].tags[1], "riemoon")
+                  assert.are.equal (data[1].attributes['riemoon'], "yes")
+                  assert.is_nil (data[1].description)
+                  assert.is_nil (data[1].host)
+                  assert.is_number (data[1].ttl)
+                  assert.is_number (data[1].time)
+                  assert.is_number (data[1].metric)
+                  assert.is_nil (data[1].unknown)
+                  assert.is_nil (data[2])
+            end)
 
-   client = riemoon.connect ("udp")
-   data, errno, err = client:query ("true")
-   assertNil (data)
-   assertEquals (errno, 95)
-end
-
-lu = LuaUnit.new ()
-lu:setOutputType ("tap")
-
-r = lu:runSuite ()
-
-collectgarbage ()
-
-os.exit (r)
+            it (
+               "Should fail to query over UDP",
+               function ()
+                  client = riemoon.connect ("udp")
+                  data, errno, err = client:query ("true")
+                  assert.is_nil (data)
+                  assert.are.equal (errno, 95)
+            end)
+      end)
+end)
